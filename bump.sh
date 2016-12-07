dcb2b63648c40e144d90246fbb7790aab1e7ab58
Travis Clarke <travis.m.clarke@gmail.com>
2016-12-06 20:52:18 -0800

    improve bump script

--- bump.sh
+++ bump.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-current_version="0.8.8"
-new_version="0.8.7"
+current_version="0.8.7"
+new_version="0.8.8"
 
-find . -type f -exec sed -i ".bak" "s/$current_version/$new_version/g" "{}" ";"
+find . -type f -not -name "*.sh" -not -path '*/\.*' -exec replace "$current_version" "$new_version" -- "{}" ";"
